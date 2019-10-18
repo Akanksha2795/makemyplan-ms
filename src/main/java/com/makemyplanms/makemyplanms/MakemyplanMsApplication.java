50975249f518d83ed87e45aebebf1c6056d4276d
Akanksha2795 <kapureakanksha21@gmail.com>
2019-10-18 08:52:17 -0700

    adding files

---
+++ src/main/java/com/makemyplanms/makemyplanms/MakemyplanMsApplication.java
@@ -0,0 +1,13 @@
+package com.makemyplanms.makemyplanms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakemyplanMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakemyplanMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makemyplanms/makemyplanms/MakemyplanMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makemyplanms.makemyplanms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemyplanMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
